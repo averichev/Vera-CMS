a3f3ae663b4258aaf085d17b1205b92812f40f24
averichev <averichev@yandex.ru>
2021-01-22 12:20:43 +0300

    chore: Identity feature in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Vera.CMS.Infrastructure.Database;
+using Vera.CMS.Infrastructure.Database.Entity;
 
 namespace Vera.CMS
 {
@@ -33,22 +35,43 @@ namespace Vera.CMS
                     .AllowAnyHeader();
             }));
             services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<DataBaseContext>();
+            services.AddIdentityServer()
+                .AddApiAuthorization<User, DataBaseContext>();
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
         }
 
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors("DefaultCorsPolicy");
-            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseIdentityServer();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
