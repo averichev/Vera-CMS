77eb5d49dac427be8f4cc469cd4d794cdbf1d1a6
averichev <averichev@yandex.ru>
2021-01-22 11:57:32 +0300

    chore: Database context changed and User entity created

--- Infrastructure/Database/DataBaseContext.cs
+++ Infrastructure/Database/DataBaseContext.cs
@@ -1,11 +1,16 @@
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Vera.CMS.Infrastructure.Database.Entity;
 
 namespace Vera.CMS.Infrastructure.Database
 {
-    public class DataBaseContext : DbContext
+    public class DataBaseContext : ApiAuthorizationDbContext<User>
     {
-        public DataBaseContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        public DataBaseContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
         
