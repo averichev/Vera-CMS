f09be3aa8a9245201c0855fef19e98f2b59877dc
averichev <averichev@yandex.ru>
2021-01-26 19:25:31 +0300

    feat: Detail page #6

---
+++ UI/Public/Models/BlockData.cs
@@ -0,0 +1,8 @@
+namespace Vera.CMS.UI.Public.Models
+{
+    public class BlockData
+    {
+        public string Text { get; set; }
+        public int Level { get; set; }
+    }
+}
---
+++ UI/Public/Models/PageView.cs
@@ -0,0 +1,8 @@
+namespace Vera.CMS.UI.Public.Models
+{
+    public class PageView
+    {
+        public string Header { get; set; }
+        public PageContent Content { get; set; }
+    }
+}
