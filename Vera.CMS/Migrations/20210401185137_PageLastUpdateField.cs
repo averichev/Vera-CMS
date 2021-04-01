2493e1cb99b4e32b52ff4e7d942925e2d562118d
averichev <averichev@yandex.ru>
2021-04-01 22:49:09 +0300

    improvement: Migration for field LastUpdateTime

---
+++ Vera.CMS/Migrations/20210401185137_PageLastUpdateField.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vera.CMS.Migrations
+{
+    public partial class PageLastUpdateField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                "LastUpdateTime",
+                "Page",
+                "timestamp",
+                nullable: true
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "Description",
+                "Page"
+                );
+        }
+    }
+}
